022c80a5fd360fbd363b53a55b16955af21b88cd
Erik Hoffman <erik.m.hoffman@gmail.com>
2021-11-08 23:16:21 -0500

    Add function for checking for valid paths

--- process_files.py
+++ process_files.py
@@ -1,17 +1,27 @@
 #!/usr/bin/env python3
 
+'''
+_author_ = Erik Hoffman
+'''
+
 import os
 
 user = input("Enter username: ")
 img = [".img",".png",".jpg"]
 doc = [".xls",".xlsx",".doc",".docx"]
 
+def check_path(file_path):
+    '''Check to see if path exists'''
+    print(file_path)
+    return os.path.isfile(path)
+
 path = "/Users/" + user +  "/Downloads/"
-files = os.listdir(path)
-for file in files:
+if check_path(path):
+  files = os.listdir(path)
+  for file in files:
     split = os.path.splitext(file)
     if split[1] != "":
-        print(split[1])
-        if split[1] in img:
-            print("FOUND!")
-            
+      if split[1] in img:
+        print("FOUND!")
+else:
+    print("Please put in a valid user")
