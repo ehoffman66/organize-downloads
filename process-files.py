d3a78ff3eeee3b67d83489ba31523c76112df58f
Erik Hoffman <erik.m.hoffman@gmail.com>
2021-11-08 22:22:27 -0500

    Add file types for images and documents

--- process-files.py
+++ process-files.py
@@ -3,6 +3,8 @@
 import os
 
 user = input("Enter username: ")
+img = [".img",".png",".jpg"]
+doc = [".xls",".xlsx",".doc",".docx"]
 
 path = "/Users/" + user +  "/Downloads/"
 files = os.listdir(path)
@@ -10,4 +12,6 @@ for file in files:
   split = os.path.splitext(file)
   if split[1] != "":
     print(split[1])
+    if split[1] in img:
+      print("FOUND!")
 
